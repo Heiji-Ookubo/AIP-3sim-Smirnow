a6305495647580ba2062c58bf9e5560ec255e1df
Heiji-Ookubo <dima.smirnow04@gmail.com>
2025-12-17 18:23:22 +0300

    Add files via upload

--- lab 2.2.c
+++ lab 2.2.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
-int main(void) {
-    printf("Hello, World!\n");
+int main() {
+    int num1, num2, nod;
+    printf("vvedite chisl: ");
+    scanf("%d %d",&num1,&num2);
+    int min_num = (num1 < num2) ? num1 : num2;
+    for (int i=2; i <= min_num; i++)
+    {
+        if (num1 %i == 0 && num2 %i == 0)
+        {
+            printf("%d i %d ne VP, nod raven %d.\n", num1, num2, i);
+            return 0;
+        }
+    }
+    printf("%d i %d VP, nod raven %d.\n", num1, num2);
     return 0;
 }
